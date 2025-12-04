949598296a9a91166bdf157286e27274a8cd74ff
Gonzalo Cerna <156675363+EmanuelCer72@users.noreply.github.com>
2025-12-04 17:30:45 -0300

    Delete TP Final directory

--- TP Final/Jugador.java
+++
@@ -1,7 +0,0 @@
-public class Jugador {
-    String nombre;
-    String simbolo;
-    public Jugador(String nombre, String simbolo) { this.nombre = nombre; this.simbolo = simbolo; }
-    public String getNombre() { return nombre; }
-    public String getSimbolo() { return simbolo; }
-}
